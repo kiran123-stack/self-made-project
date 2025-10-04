1d1d243cd409cba277067f93080b9c1dcb680073
Kiran <upliftverse98@gmail.com>
2025-10-03 22:18:19 -0700

    Update Navbar.jsx

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -9,8 +9,8 @@ const Navbar = () => {
             
             <ul flex gap-6 list-none  >
                 <li><a href='/' className=' no-underline text-white hover:text-blue-500'>Home</a></li>
-                <li><a href='/about' className='no-underline text-white hover:text-blue-500'>About</a></li>
-                <li><a href='/contact' className=' no-underline text-white hover:text-blue-500'>Contact</a></li>
+                <li><a href='#page-4' className='no-underline text-white hover:text-blue-500'>About</a></li>
+                <li><a href='/#' className=' no-underline text-white hover:text-blue-500'>Contact</a></li>
                
             </ul>
         </nav>
