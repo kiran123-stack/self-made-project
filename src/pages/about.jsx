53fc118d2a057fbf7d17680e957a47bda00fbbf0
Kiran <upliftverse98@gmail.com>
2025-10-03 22:08:27 -0700

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>A</div>
+    <div>About page is liked below the page so sorry it seems viered but this is by mistiake </div>
   )
 }
 
